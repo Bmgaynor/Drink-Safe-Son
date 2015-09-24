82d717e44aba2ae5ea96a9b292e8ef58b6fe49a3
Bmgaynor <bmgaynor@uwm.edu>
2015-09-24 12:28:27 -0500

    Index page fix

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-}).get('/Drinks', function(req, res, next){
-  res.json({
-    "name":"Brandy Old Fashion",
-    "description":"Sweet Drink for the winter that tastes like home.",
-    "ingredients":{
-      "alcohol":"brandy",
-      "fruit":"cherry"
-    }
-  })
+  res.render('index',{ title: 'Drink express'});
 });
 
 module.exports = router;
